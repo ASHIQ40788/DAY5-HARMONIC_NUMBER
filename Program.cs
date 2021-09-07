fb2a40d8b325f99532b11394414837fd229d0206
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-07 22:01:46 +0530

    [ASHIQ] added : Nth HARMONIC NUMBER

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace HARMONIC_NUMBER
+{
+    class NTH_HARMONIC_NUMBER
+    {
+
+        // Function to return sum of harmonic series
+        static float sum(int n)
+        {
+            double i, s = 0.0;
+            
+            //s=sum
+            for (i = 1; i <= n; i++)
+                s = s + 1 / i;
+
+            return (float)s;
+        }
+
+
+        // Driven Program
+        public static void Main()
+        {
+            int n = 100;
+            Console.WriteLine("Sum is " + sum(n));
+            Console.Read();
+        }
+    }
+}
